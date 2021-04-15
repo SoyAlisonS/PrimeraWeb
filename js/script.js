ce6860e245a49de4ca8dc7dbfaf1b19a1a40b2cc
SoyAlisonS <alisonsarriam@gmail.com>
2021-04-14 21:44:00 -0500

    contactenos y alertas JS

---
+++ js/script.js
@@ -0,0 +1,29 @@
+function validardatos() {
+   /*
+    alert("Hola mundo");
+    prompt("Ingrese su nombre"); //alerta con input//
+    console.log("Hola mundo") //sale en la consola, INSPECCIONAR //
+    document.write("Hola mundo"); //se escribe en el documento, para que salga en pantalla... cunando se le da clic en el boton//
+*/
+
+let nombre = document.getElementById("name").value
+
+alert(nombre)
+
+let email = document.getElementById("email").value
+
+alert(email)
+
+let edad = document.getElementById("edad").value
+
+alert(edad)
+
+let telefono = document.getElementById("telefono").value
+
+alert(telefono)
+
+let comentarios = document.getElementById("comentarios").value
+
+alert(comentarios)
+
+}
